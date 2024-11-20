d2a0fa02cd2bfb5cdebc5c72e777d52dc5812721
rory lange <rorystrattonlange@gmail.com>
2024-11-20 17:11:28 -0500

    put the router in there

--- scheduler-web/src/App.tsx
+++ scheduler-web/src/App.tsx
@@ -1,15 +1,17 @@
 import './styles/App.scss'
 import Login from './components/Login/Login';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <div>
-        <Login />
-      </div>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
