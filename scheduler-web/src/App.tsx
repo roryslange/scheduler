4c4aff2ea912dd33897c5c732c7724e192882795
rory lange <rorystrattonlange@gmail.com>
2024-11-21 17:47:06 -0500

    create home route

--- scheduler-web/src/App.tsx
+++ scheduler-web/src/App.tsx
@@ -1,21 +1,21 @@
 import './styles/App.scss'
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Home from './pages/Home/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext/AuthContext';
 
 function App() {
   return (
-    <div>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
